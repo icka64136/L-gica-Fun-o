480f98d455b4cacf7c55ba87269bbc55cea66965
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-11-12 21:38:36 -0300

    Dia 1

--- Att lista 1 exe 3.c
+++ Att lista 1 exe 3.c
@@ -1,4 +1,4 @@
-/* Elabore um programa que ap�s ler um valor verifique se � par ou
+/* Elabore um programa que após ler um valor verifique se é par ou
 impar. Se for par multiplique por 5 e se for impar divida por 3 e mostre
 na tela */
 #include<stdio.h>
@@ -7,7 +7,7 @@ par(int z)
 {
 	if(z%2==0)
 	{
-		 printf("Par : %d",z*5);
+		 printf("Par : %d",z*5); //O Printf dentro da Função serve para chamar completamente a Função
 	}
 	else
 	{
